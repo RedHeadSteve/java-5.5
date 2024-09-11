68a94ba43267ce90c972bca0cb2bae606706337b
RedHeadSteve <veermansteven99@gmail.com>
2024-09-11 13:47:57 +0200

    Opdracht gemaakt

---
+++ src/main/java/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+     String carBrand;
+     String carType;
+     int carBuildYear;
+     String carColor;
+     float carEngineCapacity;
+
+     public Car (String carBrand, String carType, int carBuildYear, String carColor, float carEngineCapacity) {
+          this.carBrand = carBrand;
+          this.carType = carType;
+          this.carBuildYear = carBuildYear;
+          this.carColor = carColor;
+          this.carEngineCapacity = carEngineCapacity;
+     }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        Car newCarFiat = new Car("Fiat", "500", 2019, "groen", 1.4F);
+        Car newCarToyota = new Car("Toyota", "Aygo", 2016, "blauw", 1.2F);
+        Car newCarVolvo = new Car("Volvo", "CX90", 2022, "zwart", 2.0F);
+
+        System.out.println(newCarFiat.carBrand + " " + newCarFiat.carType + " " + newCarFiat.carBuildYear + " " + newCarFiat.carColor + " " + newCarFiat.carEngineCapacity);
+    }
 }
